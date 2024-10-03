660d70726ae1388c6712618bab07447c782a4304
Francisco Collazos <fcollazos@internetbrands.com>
2024-10-03 10:09:00 -0400

    setup react router
    * install react router
    * add router provider and page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,21 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from 'routes/Root';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <div>Not found</div>
+  }
+]);
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div>Hello!</div>
+      <RouterProvider router={router} />
     </QueryClientProvider>
   );
 }
